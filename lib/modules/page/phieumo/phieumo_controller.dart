45fddabacfb11f758ae91380e7b18fc9317c7f56
quangnm.pto <quangnm.pto@vnpt.vn>
2024-12-23 13:05:07 +0700

    điều chỉnh cấu trúc chương trình

--- lib/modules/page/phieumo/phieumo_controller.dart
+++ lib/modules/page/phieumo/phieumo_controller.dart
@@ -1,15 +1,235 @@
 import 'dart:convert';
+import 'dart:ffi';
 
 import 'package:nms_app/core/values/get_storage_key.dart';
-import 'package:nms_app/modules/page/phieumo/phieumo_provider.dart';
+import 'package:nms_app/model/ultis/dsdoituong.dart';
+import 'package:nms_app/model/ultis/dsnguyennhan.dart';
+import 'package:nms_app/model/ultis/thuebaohong.dart';
+import 'package:flutter/widgets.dart';
 import 'package:get/get.dart';
 import 'package:get_storage/get_storage.dart';
+import 'package:nms_app/provider/quan-ly-tin-bai/ban_tin_provider.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
-class PhieumoController extends GetxController {
+class PhieumoController extends GetxController
+    with StateMixin<List<thuebaohong>> {
+  final bantinprovider = BanTinProvider();
   var storage = GetStorage();
   var choicebutton = "chuaxuly".obs;
+  List<Dsnguyennhan> dsnguyennhan = <Dsnguyennhan>[].obs;
+  List<Dsdoituong> dsdoituong = <Dsdoituong>[].obs;
+  List<thuebaohong> dsthuebaofull = <thuebaohong>[].obs;
+  List<thuebaohong> dsthuebao = <thuebaohong>[].obs;
+  List<thuebaohong> dsthuetam = <thuebaohong>[].obs;
+
+  int NNDuocChon = -1;
+  int DTDuocChon = -1;
+  int Trangthaixl = 0;
+  ScrollController scrollerControllerChuaXuLy = ScrollController();
+  ScrollController scrollerControllerDaXuLy = ScrollController();
+
+  RxBool isloading = true.obs;
+  int page = 0;
+  int maxpage = 1;
+
   @override
-  void onInit() {
+  void onInit() async {
     super.onInit();
+    GetDsnguyennhan();
+    GetDsDoiTuong();
+    getdsbaohong();
+  }
+
+  @override
+  void onReady() {
+    super.onReady();
+    loaddata();
+  }
+
+  @override
+  void onClose() {
+    super.onClose();
+    scrollerControllerChuaXuLy.dispose();
+    scrollerControllerDaXuLy.dispose();
+  }
+
+  void loaddata() {
+    getdsbaohong();
+    loadMoreds();
+  }
+
+  @override
+  void dispose() {
+    scrollerControllerChuaXuLy.removeListener(_scrollListenerhuaXuLy);
+    scrollerControllerChuaXuLy.dispose();
+
+    scrollerControllerDaXuLy.removeListener(_scrollerControllerDaXuLy);
+    scrollerControllerDaXuLy.dispose();
+
+    super.dispose();
+  }
+
+  void loadMoreds() {
+    try {
+      scrollerControllerChuaXuLy.addListener(_scrollListenerhuaXuLy);
+      // ignore: empty_catches
+    } catch (exception) {}
+  }
+
+  void getMoreds() {
+    if ((page + 1) * 20 > dsthuetam.length) {
+      for (int i = page * 20; i < dsthuetam.length; i++) {
+        dsthuebao.add(dsthuetam[i]);
+      }
+    } else {
+      for (int i = page * 20; i < (page + 1) * 20; i++) {
+        dsthuebao.add(dsthuetam[i]);
+      }
+    }
+    change(dsthuebao, status: RxStatus.success());
+  }
+
+  void loadmoredata() {
+    scrollerControllerChuaXuLy.removeListener(_scrollListenerhuaXuLy);
+    loadMoreds();
+  }
+
+  void filterSearchResults(String search) {
+    page = 0;
+    if (search == "") {
+      dsthuebao.clear();
+      dsthuetam = dsthuebaofull;
+      getMoreds();
+    } else {
+      dsthuebao.clear();
+      dsthuetam = dsthuebaofull
+          .where((item) =>
+              sosanhten(item.ten_tb ?? "", search) ||
+              sosanhten(item.ma_tb ?? "", search))
+          .toList();
+      getMoreds();
+    }
+  }
+
+  bool sosanhten(String char1, String char2) {
+    if (char1.toUpperCase().indexOf(char2.toUpperCase()) > -1) {
+      return true;
+    }
+    return false;
+  }
+
+  void _scrollListenerhuaXuLy() async {
+    if (scrollerControllerChuaXuLy.position.maxScrollExtent ==
+        scrollerControllerChuaXuLy.position.pixels) {
+      if (page <= maxpage) {
+        page++;
+        getMoreds();
+      }
+    }
+  }
+
+  void _scrollerControllerDaXuLy() {
+    if (scrollerControllerDaXuLy.position.maxScrollExtent ==
+        scrollerControllerDaXuLy.position.pixels) {
+      if (page <= maxpage) {
+        page++;
+        getMoreds();
+      }
+    }
+  }
+
+  void ChonTrangThaiXuLy(int trangthai) {
+    if (Trangthaixl != trangthai) {
+      page = 0;
+
+      NNDuocChon = -1;
+      DTDuocChon = -1;
+      if (trangthai == 1) {
+        choicebutton.value = "daxuly";
+        Trangthaixl = 1;
+      } else {
+        choicebutton.value = "chuaxuly";
+        Trangthaixl = 0;
+      }
+      loaddata();
+    }
+  }
+
+  void getdsbaohong() async {
+    final SharedPreferences pref = await SharedPreferences.getInstance();
+    isloading.value = true;
+    String userName = pref.getString(GetStorageKey.userName)!;
+    String passWord = pref.getString(GetStorageKey.passWord)!;
+    int trangthaixl = Trangthaixl;
+    int ttphieu = 0;
+    String maNV = pref.getString(GetStorageKey.manhanvien)!;
+    int nhomquyen = pref.getInt(GetStorageKey.nhomquyen)!;
+    int donvi = pref.getInt(GetStorageKey.donviId)!;
+    int loaidttb = NNDuocChon;
+    int loaikh = DTDuocChon;
+
+    change(null, status: RxStatus.loading());
+    try {
+      bantinprovider
+          .dsBaoHong(userName, passWord, trangthaixl, ttphieu, maNV, nhomquyen,
+              donvi, loaidttb, loaikh)
+          .then((value) {
+        isloading.value = false;
+        dsthuebaofull.clear();
+        dsthuebao.clear();
+        value.forEach((e) {
+          dsthuebaofull.add(e);
+        });
+        dsthuetam = dsthuebaofull;
+        maxpage = (value.length / 20).toInt();
+        if (maxpage * 20 < value.length) maxpage++;
+        getMoreds();
+      });
+    } catch (e) {
+      isloading.value = false;
+      print('lỗi: $e');
+    }
+  }
+
+  void GetDsnguyennhan() async {
+    await bantinprovider.dsnguyennhan().then((value) {
+      Dsnguyennhan tatca = Dsnguyennhan();
+      tatca.nn_id = -1;
+      tatca.ten_nn = "Nguyên nhân: Tất cả";
+      NNDuocChon = -1;
+      dsnguyennhan.add(tatca);
+      value.forEach((e) {
+        e.ten_nn = "Nguyên nhân: " + e.ten_nn.toString();
+        dsnguyennhan.add(e);
+      });
+    });
+  }
+
+  void GetDsDoiTuong() async {
+    await bantinprovider.dsdoituong().then((value) {
+      Dsdoituong tatca = Dsdoituong();
+      tatca.loai_kh_id = -1;
+      tatca.ten_loai_kh_id = "Đối tượng: Tất cả";
+      tatca.mau_sms = "null";
+
+      DTDuocChon = -1;
+      dsdoituong.add(tatca);
+      value.forEach((e) {
+        e.ten_loai_kh_id = "Đối tượng: " + e.ten_loai_kh_id.toString();
+        dsdoituong.add(e);
+      });
+    });
+  }
+
+  void ChangeNguyenNhan(String idNn) {
+    page = 0;
+    NNDuocChon = int.parse(idNn);
+    loaddata();
+  }
+
+  void ChangeDoiTuong(String ten_loai_kh_id) {
+    DTDuocChon = int.parse(ten_loai_kh_id);
+    page = 0;
+    loaddata();
   }
 }
--- lib/modules/page/phieumo/phieumo_view.dart
+++ lib/modules/page/phieumo/phieumo_view.dart
@@ -1,5 +1,9 @@
 import 'package:nms_app/core/values/app_color.dart';
+import 'package:nms_app/core/values/app_theme_values.dart';
 import 'package:nms_app/global_widget/button_custom.dart';
+import 'package:nms_app/model/ultis/dsdoituong.dart';
+import 'package:nms_app/model/ultis/dsnguyennhan.dart';
+import 'package:nms_app/modules/items/items_ds_phieumo.dart';
 import 'package:nms_app/modules/page/phieumo/phieumo_controller.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_spinkit/flutter_spinkit.dart';
@@ -11,48 +15,182 @@ import 'dart:ui';
 class PhieumoView extends GetView<PhieumoController> {
   @override
   Widget build(BuildContext context) {
-    return GestureDetector(
+    return Container(
       child: Scaffold(
-        body: SingleChildScrollView(
-          child: OrientationBuilder(
-              builder: (BuildContext context, Orientation orientation) {
-            return Container(
-                child: Column(children: [
-              Padding(
-                padding: EdgeInsets.only(top: 5, left: 5, right: 5),
-                child: Center(
-                  child: Row(
-                    children: [
-                      ButtonCustom(
-                        title: "Chưa xử lý",
-                        onPressed: () {},
-                        backgroundColor:
-                            controller.choicebutton.value == "chuaxuly"
-                                ? AppColor.blueAccentColor
-                                : AppColor.whiteColor,
-                        textColor: controller.choicebutton.value == "chuaxuly"
-                            ? AppColor.whiteColor
-                            : AppColor.blueAccentColor,
+        backgroundColor: Colors.white,
+        body: OrientationBuilder(
+            builder: (BuildContext context, Orientation orientation) {
+          return Container(
+              color: AppColor.whiteColor,
+              child: Column(children: [
+                Container(
+                    height: 250,
+                    child: Column(children: [
+                      Padding(
+                        padding: EdgeInsets.only(top: 5, left: 5, right: 5),
+                        child: Center(
+                            child: Obx(
+                          () => Row(
+                            mainAxisAlignment: MainAxisAlignment.center,
+                            children: [
+                              ButtonCustom(
+                                title: "Chưa xử lý",
+                                onPressed: () {
+                                  controller.ChonTrangThaiXuLy(0);
+                                },
+                                backgroundColor:
+                                    controller.choicebutton.value == "chuaxuly"
+                                        ? AppColor.blueAccentColor
+                                        : AppColor.whiteColor,
+                                textColor:
+                                    controller.choicebutton.value == "chuaxuly"
+                                        ? AppColor.whiteColor
+                                        : AppColor.blueAccentColor,
+                              ),
+                              SizedBox(width: 5.0),
+                              ButtonCustom(
+                                title: "Đã xử lý",
+                                onPressed: () {
+                                  controller.ChonTrangThaiXuLy(1);
+                                },
+                                backgroundColor:
+                                    controller.choicebutton.value != "chuaxuly"
+                                        ? AppColor.blueAccentColor
+                                        : AppColor.whiteColor,
+                                textColor:
+                                    controller.choicebutton.value != "chuaxuly"
+                                        ? AppColor.whiteColor
+                                        : AppColor.blueAccentColor,
+                              )
+                            ],
+                          ),
+                        )),
                       ),
-                      ButtonCustom(
-                        title: "Đã xử lý",
-                        onPressed: () {},
-                        backgroundColor:
-                            controller.choicebutton.value != "chuaxuly"
-                                ? AppColor.blueAccentColor
-                                : AppColor.whiteColor,
-                        textColor: controller.choicebutton.value != "chuaxuly"
-                            ? AppColor.whiteColor
-                            : AppColor.blueAccentColor,
+                      Padding(
+                          padding: EdgeInsets.only(top: 5, left: 10, right: 10),
+                          child: dropNguyenNhan(context)),
+                      Padding(
+                          padding: EdgeInsets.only(top: 5, left: 10, right: 10),
+                          child: dropDoiTuong(context)),
+                      Padding(
+                        padding: EdgeInsets.only(top: 5, left: 10, right: 10),
+                        child: TextFormField(
+                          decoration: InputDecoration(
+                              hintText: 'Nhập thông tin tìm kiếm...',
+                              border: OutlineInputBorder(
+                                borderRadius: BorderRadius.circular(10),
+                              ),
+                              prefixIcon: Icon(Icons.search),
+                              filled: true, // Bật chế độ làm nền có màu
+                              fillColor: AppColor.greyColor),
+                          onFieldSubmitted: ((SearchValue) {
+                            controller
+                                .filterSearchResults(SearchValue.toString());
+                          }),
+                        ),
                       )
-                    ],
-                  ),
-                ),
-              )
-            ]));
-          }),
-        ),
+                    ])),
+                Expanded(
+                    child: Obx(() => (controller.isloading.value == true)
+                        ? Padding(
+                            padding: EdgeInsets.only(top: 5, left: 5, right: 5),
+                            child: WillPopScope(
+                                child: Center(
+                                  child: SpinKitCircle(
+                                      color: Get.isDarkMode
+                                          ? AppColor.yellowColor
+                                          : AppColor.blueAccentColor,
+                                      size: 40),
+                                ),
+                                onWillPop: () => Future.value(false)),
+                          )
+                        : 
+                            controller.obx(
+                        (dsphieu) => RefreshIndicator(
+                                onRefresh: () async {
+                                  controller.loadmoredata();
+                                },
+                                child: ListView.builder(
+                                    controller:
+                                        controller.scrollerControllerChuaXuLy,
+                                    physics:
+                                        const AlwaysScrollableScrollPhysics(),
+                                    itemCount: dsphieu?.length,
+                                    itemBuilder: ((context, index) =>
+                                        ItemListPhieu(
+                                            thuebao:
+                                                controller.dsthuebao[index],
+                                            onClickItem: () {})))),
+                          )))
+              ]));
+        }),
       ),
     );
+  }
+
+  Widget dropNguyenNhan(BuildContext context) {
+    return Obx(() => DropdownButtonFormField<String>(
+          isExpanded: true,
+          value: controller.NNDuocChon.toString(),
+          onChanged: (String? newValue) {
+            controller.ChangeNguyenNhan(newValue.toString());
+          },
+          items: controller.dsnguyennhan
+              .map<DropdownMenuItem<String>>((Dsnguyennhan value) {
+            return DropdownMenuItem<String>(
+              value: value.nn_id.toString() ?? "-1",
+              child: Text(
+                value.ten_nn ?? "",
+                maxLines: 1,
+                overflow: TextOverflow.ellipsis,
+              ),
+            );
+          }).toList(),
+          validator: (value) {
+            if (value == null) {
+              return 'Vui lòng chọn một mục';
+            }
+            return null;
+          },
+          decoration: InputDecoration(
+            border: OutlineInputBorder(),
+          ),
+          style: TextStyle(
+              color: AppColor.blueAccentColor,
+              fontSize: AppSizeValues.FontSizeSmall),
+        ));
+  }
+
+  Widget dropDoiTuong(BuildContext context) {
+    return Obx(() => DropdownButtonFormField<String>(
+          isExpanded: true,
+          value: controller.DTDuocChon.toString(),
+          onChanged: (String? newValue) {
+            controller.ChangeDoiTuong(newValue.toString());
+          },
+          items: controller.dsdoituong
+              .map<DropdownMenuItem<String>>((Dsdoituong value) {
+            return DropdownMenuItem<String>(
+              value: value.loai_kh_id.toString() ?? "-1",
+              child: Text(
+                value.ten_loai_kh_id ?? "",
+                maxLines: 1,
+                overflow: TextOverflow.ellipsis,
+              ),
+            );
+          }).toList(),
+          validator: (value) {
+            if (value == null) {
+              return 'Vui lòng chọn một mục';
+            }
+            return null;
+          },
+          decoration: InputDecoration(
+            border: OutlineInputBorder(),
+          ),
+          style: TextStyle(
+              color: AppColor.blueAccentColor,
+              fontSize: AppSizeValues.FontSizeSmall),
+        ));
   }
 }
